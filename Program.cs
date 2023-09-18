b85e79ade56c0401998adf345cb840896a1f7d87
Viking <kmss13@mail.ru>
2023-09-18 21:00:11 +0400

    add solution of the hometask

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] array1 = { "1234", "1567", "-2", "computer science","123" };
+string[] array2 = new string[array1.Length];
+int count = 0;
+
+
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array2);
